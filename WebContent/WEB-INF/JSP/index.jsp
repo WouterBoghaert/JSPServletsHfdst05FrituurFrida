8ae4141baf1e2b9980ff2786e70afd3db6ab659a
WouterBoghaert <wouter.boghaert@gmail.com>
2017-10-27 15:14:37 +0200

    Hoofdstuk 8

--- WebContent/WEB-INF/JSP/index.jsp
+++ WebContent/WEB-INF/JSP/index.jsp
@@ -1,6 +1,5 @@
 <%@ page contentType="text/html" pageEncoding="UTF-8" session="false"%>
-<%@ page import="java.time.LocalDate" %>
-<%@ page import="java.time.DayOfWeek" %>
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -11,12 +10,6 @@
 </head>
 <body>
 	<h1>Frituur Frida</h1>
-	<h2>
-	 Vandaag zijn we 
-	 <%
-	 DayOfWeek vandaag = LocalDate.now().getDayOfWeek();
-	out.print(vandaag == DayOfWeek.MONDAY||vandaag == DayOfWeek.THURSDAY ? "gesloten" : "open");
-	 %>
-	</h2>
+	<h2>Vandaag zijn we ${openGesloten}</h2>
 </body>
 </html>
--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -1,7 +1,6 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 
@@ -14,33 +13,15 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class IndexServlet
  */
-//@WebServlet("/index.htm")
+@WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html lang='nl'>");
-		out.println("<head><meta charset='UTF-8'>");
-	out.println("<title>Frituur Frida</title>");
-	out.println("<link rel='icon' href='images/favicon.ico'>");
-	out.println("<meta name='viewport' content='width=device-width,initial-scale=1'>");
-	out.println("<link rel='stylesheet' href='styles/frituurfrida.css'>");
-	out.println("</head><body><h1>Frituur Frida</h1><h2>");
-//	String vandaag = LocalDate.now().getDayOfWeek().toString();
-//	out.println(vandaag.equalsIgnoreCase("monday")||vandaag.equalsIgnoreCase("thursday") ? "Vandaag zijn we gesloten" :
-//			"Vandaag zijn we open");
 	DayOfWeek vandaag = LocalDate.now().getDayOfWeek();
-	out.println("Vandaag zijn we " + (vandaag == DayOfWeek.MONDAY||vandaag == DayOfWeek.THURSDAY ? "gesloten" : "open"));
-	out.println("</h2></body></html>");
-	//System.out.println(vandaag);
-	
-	
+	request.setAttribute("openGesloten", vandaag == DayOfWeek.MONDAY||vandaag == DayOfWeek.THURSDAY ? "gesloten" : "open");
+	request.getRequestDispatcher(VIEW).forward(request, response);
 	}
-
 }
